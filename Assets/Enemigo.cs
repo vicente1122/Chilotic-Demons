ce7972edb61e5b9012317f622a17fea23f229701
EloyPrado <eloy0prado0jr@gmail.com>
2020-12-17 16:29:11 -0300

    Ver vida, criticos
    -Se puede ver como la vida del enemigo baja
    -Los enemigos tienen "golpes criticos"

--- Assets/Enemigo.cs
+++ Assets/Enemigo.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemigo : MonoBehaviour
 {
     public float dimension_x;    //para evitar problemas de orientacion y asignar las dimensiones
     public float dimension_y;
-    public int salud;  //total de vida
+    public int salud_inicial; //total de vida inicial, no se cambia
+    public int salud;  //total de vida, este se cambia
     public float velocidad;
     public float distancia;     //a que distancia detecta el suelo
-    private bool avanzandoIzquierda = true;
-    private Rigidbody2D rb2d;
     public Transform deteccionSuelo;
+    private float velocidad2;
+    private Rigidbody2D rb2d;
+    private bool avanzandoIzquierda = true;
 
     //variables de combate abajo
     public float Vista; //rango visual
@@ -19,13 +22,17 @@ public class Enemigo : MonoBehaviour
     public int daño; //daño MINIMO que puede hacer
     public LayerMask que_es_objetivo; //se asigna quien califica como enemigo
     public Transform PosAtaque; //se donde viene el ataque
-    private float tiempoEntreAtaque; //cuenta el tiempo que pasa
     public float IniciartiempoEntreAtaque; //cuenta el tiempo que pasa entre ataques
     public Transform player;
+    private float tiempoEntreAtaque; //cuenta el tiempo que pasa
+
+    public Image vida;    // Para desplegar la vida de forma visible
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        velocidad2 = velocidad;
+        salud_inicial = salud;
     }
 
     // Update is called once per frame
@@ -62,7 +69,7 @@ public class Enemigo : MonoBehaviour
                 transform.localScale = new Vector3(-dimension_x, dimension_y, 0);
             }
         }
-
+        //agregar una forma de detectar y seguir al jugador
         if (tiempoEntreAtaque <= 0)   //CD para los ataques
         {
             Collider2D[] enemigosDañados = Physics2D.OverlapCircleAll(PosAtaque.position, Rango, que_es_objetivo);
@@ -71,8 +78,18 @@ public class Enemigo : MonoBehaviour
                 if (enemigosDañados.Length > 0)
                 {
                     tiempoEntreAtaque = IniciartiempoEntreAtaque;
+                    int num = Random.Range(1, 101);          //probabilidades
+                    if (num < 90)            //ataque normal
+                    {
+                        enemigosDañados[i].GetComponent<player>().RecibeDaño(daño);
+                    }
+                    else if (num > 89)     //golpe critico
+                    {
+                        int critico = daño * 3;
+                        enemigosDañados[i].GetComponent<player>().RecibeDaño(critico);
+                        Debug.Log("Golpe Critico");
+                    }
                 }
-                enemigosDañados[i].GetComponent<player>().RecibeDaño(daño);
             }
         }
         else
@@ -89,5 +106,11 @@ public class Enemigo : MonoBehaviour
         {
             Destroy(this.gameObject);            //muere *agreguen animaciones porfa*
         }
+        //float proporcion = salud_inicial / d;
+        float proporcion = (float)salud / (float)salud_inicial;
+        Debug.Log(salud);
+        Debug.Log(salud_inicial);
+        Debug.Log(salud/salud_inicial);
+        vida.fillAmount = proporcion;
     }
 }
