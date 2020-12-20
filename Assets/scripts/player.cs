9adbd4c280f6f58ab1b120d19d44c7994fa894bc
vicente1122 <1122viluoz@gmail.com>
2020-12-20 19:44:04 -0300

    Revert "Revert "Merge branch 'main' of https://github.com/vicente1122/Chilotic-Demons into main""
    This reverts commit 88c2039b69451ca3f2dabf46e7421094cfc1c1d4.

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
+    public int evasion; //posibilidad de esquivar ataques en %
     public int salud;  //total de vida
     public float speed;
     public float jump_power;
@@ -12,12 +13,20 @@ public class player : MonoBehaviour
     public KeyCode right;
     public KeyCode jump;
     public KeyCode jumpW;
+    public KeyCode Ataque1;
+    public KeyCode Ataque2;
+
+    public KeyCode Shake;
     private Rigidbody2D rb2d;   
     public bool ground;
     private Animator anim;
     public ParticleSystem polvo;
 
+    public CameraFollow camFollow;
+    private bool ax1Fire;
+    private bool ax2Fire;
     public Text textoSalud;
+    public float tiempo;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,11 +41,17 @@ public class player : MonoBehaviour
         
         
     }
+    public void timer(float duracion)
+    {
+        tiempo=duracion;
+    }
     void FixedUpdate()
     {
         
         anim.SetBool("ground",ground);
         anim.SetFloat("speed",Mathf.Abs(rb2d.velocity.x));
+        anim.SetBool("axFire1",ax1Fire);
+        anim.SetBool("axFire2",ax2Fire);
         
         if (Input.GetKey(left)){
             if(ground) CrearPolvo();
@@ -56,21 +71,74 @@ public class player : MonoBehaviour
             CrearPolvo();
             rb2d.velocity=new Vector2(rb2d.velocity.x,jump_power);
         }
+        
+        if (Input.GetKey(Ataque1)&&ground)
+        {
+            //camFollow.ShakeCamera(1f,0.1f);
+            timer(0.5f);
+            if(tiempo>=0)
+            {
+                ax1Fire=true;
+                rb2d.velocity= new Vector2(0,0);
+                tiempo-=Time.deltaTime;
+            }
+            
+        }
+        else if(Input.GetKey(Ataque2)&&ground)
+        {
+            
+            timer(0.8f);
+            if(tiempo>=0)
+            {
+                ax2Fire=true;
+                rb2d.velocity= new Vector2(0,0);
+                tiempo-=Time.deltaTime;
+                
+            }
+            
+        }
+        if (ax1Fire&&tiempo<=0.5f && tiempo>0)
+        {
+            camFollow.ShakeCamera(0.5f,0.01f);
+        }
+        else if (ax2Fire&&tiempo<=0.3f && tiempo>0)
+        {
+            camFollow.ShakeCamera(1f,0.1f);
+        }
+        if (tiempo<=0)
+        {
+            ax1Fire=false;
+            ax2Fire=false;
+/*=======/*
+        if (Input.GetKey(Shake)){
+            camFollow.ShakeCamera(1.5f,0.1f);
+>>>>>>> 09737284ff1e12eb93eff95972da9adc3e7e63d5
+        }*/
+    }
     }
     void CrearPolvo()
     {
         polvo.Play();
     }
+    
     public void RecibeDaño(int d)         //cuando recibe daño
     {
-        salud -= d;
-        Debug.Log("daño recibido");
-        if (salud <= 0)                      //si pierde toda su vida
+        int num = Random.Range(1, 101); //probablidades
+        if (num < evasion) //logra evadir
         {
-            Debug.Log("moriste");
-            salud = 0;
-            Destroy(this.gameObject);            //muere *agregen animaciones porfa*
+            d = d * 0;    //esquiva ataque
+            Debug.Log("esquivaste un ataque");
         }
-        textoSalud.text = "Salud:" + salud.ToString();
-    }
-}
+        else {
+            salud -= d;
+            Debug.Log("daño recibido");
+            if (salud <= 0)                      //si pierde toda su vida
+            {
+                Debug.Log("moriste");
+                salud = 0;
+                Destroy(this.gameObject);            //muere *agregen animaciones porfa*
+            }
+            textoSalud.text = "Salud:" + salud.ToString();
+        }
+    }}
+
