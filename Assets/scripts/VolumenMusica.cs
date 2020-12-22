fd3d017e86caacde2f0180e79deefa3f17be7207
EloyPrado <eloy0prado0jr@gmail.com>
2020-12-22 20:34:12 -0300

    Controles de volumen
    -Control de volmen funcional en Menu Principal
    -El volumen aplica a TODOS los niveles una vez puesto
    -Solo se puede modificar en el menu principal

---
+++ Assets/scripts/VolumenMusica.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumenMusica : MonoBehaviour
+{
+    public AudioSource fuente;
+    public Slider deslizadorVol;
+    private float volumen = 1f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //fuente = GetComponent<AudioSource>();
+        volumen = PlayerPrefs.GetFloat("volum");
+        fuente.volume = volumen;
+        deslizadorVol.value = volumen;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        fuente.volume = volumen;
+        PlayerPrefs.SetFloat("volum", volumen);
+    }
+    public void actualizarVolumen(float vol)
+    {
+        volumen = vol;
+    }
+}
