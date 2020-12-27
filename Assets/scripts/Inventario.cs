380e93da4c6e273fda9d4069e066afe6b2e88e5e
vicente1122 <1122viluoz@gmail.com>
2020-12-27 00:05:08 -0300

    inventario avance

--- Assets/scripts/Inventario.cs
+++ Assets/scripts/Inventario.cs
@@ -6,14 +6,55 @@ public class inventario : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+    public GameObject selectedSlot;
+    public KeyCode Next;
+    public KeyCode Prev;
+    private int selectedSlotInt=0;
         void Start()
     {
-        
+        Instantiate(selectedSlot,slots[0].transform,false);
+        Instantiate(selectedSlot,slots[1].transform,false);
+        Instantiate(selectedSlot,slots[2].transform,false);
+        //slots[0].transform.GetChild(0).gameObject.SetActive(false);
+        slots[1].transform.GetChild(0).gameObject.SetActive(false);
+        slots[2].transform.GetChild(0).gameObject.SetActive(false);
     }
 
    
-    void Update()
+    void FixedUpdate()
     {
         
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            slots[selectedSlotInt].transform.GetChild(0).gameObject.SetActive(false);
+
+            
+            if (selectedSlotInt+1>slots.Length-1)
+            {
+                selectedSlotInt=0;
+            }
+            else
+            {
+                selectedSlotInt+=1;;
+            }
+            Debug.Log(selectedSlotInt);
+            slots[selectedSlotInt].transform.GetChild(0).gameObject.SetActive(true);
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            slots[selectedSlotInt].transform.GetChild(0).gameObject.SetActive(false);
+            
+            if (selectedSlotInt-1<0)
+            {
+                selectedSlotInt=slots.Length-1;
+                Debug.Log(selectedSlotInt);
+            }
+            else
+            {
+                selectedSlotInt-=1;
+            }
+            Debug.Log(selectedSlotInt);
+            slots[selectedSlotInt].transform.GetChild(0).gameObject.SetActive(true);
+        }
     }
 }
