61619dc0feb7957d806c05a63e4e1d7d90e8e4e5
EloyPrado <eloy0prado0jr@gmail.com>
2021-01-05 16:30:40 -0300

    -Sistema de daño
    -Esqueleto y Araña ahora inflijen y reciben daño
    -Estan listos para copiar y pegar a otras escenas

--- Assets/scripts/SpiderFollow.cs
+++ Assets/scripts/SpiderFollow.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpiderFollow : MonoBehaviour
 {
+    public int salud;  //total de vida, este se cambia
+    public LayerMask que_es_objetivo; //se asigna quien califica como enemigo
+    public float IniciartiempoEntreAtaque; //cuenta el tiempo que pasa entre ataques
+    public Transform player;  
+    public float Rango; //rango de ataque
+    public Transform PosAtaque; //se donde viene el ataque
+    public int daño; //daño MINIMO que puede hacer
+    public Image vida;    // Para desplegar la vida de forma visible
+    private int salud_inicial; //total de vida inicial, no se cambia
+    private float tiempoEntreAtaque; //cuenta el tiempo que pasa
+
     private Vector2 Velocity;
     public float SmoothY;
     public float SmoothX;
@@ -33,7 +45,8 @@ public class SpiderFollow : MonoBehaviour
         MatDefault=spriteRenderer.material;
         //col.gameObject.SetActive(true);
         tiempo=-1f;
-        
+
+        salud_inicial = salud;
     }
 
     // Update is called once per frame
@@ -43,62 +56,81 @@ public class SpiderFollow : MonoBehaviour
     }
     void FixedUpdate()
     {
-        anim.SetBool("run",running);
+        anim.SetBool("run", running);
         /*anim.SetFloat("speed",tiempo);
         anim.SetBool("ataque",ataque);
         anim.SetBool("reanim",reanim);*/
-        float posX=Mathf.SmoothDamp(transform.position.x,Player1.transform.position.x,ref Velocity.x,SmoothX);
-        float posY=Mathf.SmoothDamp(transform.position.y,Player1.transform.position.y,ref Velocity.y,SmoothY);
-        
+        float posX = Mathf.SmoothDamp(transform.position.x, Player1.transform.position.x, ref Velocity.x, SmoothX);
+        float posY = Mathf.SmoothDamp(transform.position.y, Player1.transform.position.y, ref Velocity.y, SmoothY);
 
-        
+
+
         //Debug.Log(Mathf.Abs(transform.position.x-Player1.transform.position.x));
 
         //Debug.Log(tiempo);
-        if(tiempo>0)
+        if (tiempo > 0)
         {
-            reanim=true;/**/
-            tiempo-=Time.deltaTime;
+            reanim = true;/**/
+            tiempo -= Time.deltaTime;
         }
-        else if (tiempo<=0)
+        else if (tiempo <= 0)
         {
-            reanim=false;
-            tiempo=0f;
+            reanim = false;
+            tiempo = 0f;
             //col.gameObject.SetActive(false);
         }
         /*if (ataque==true)
         {
             tiempo=0.5f;
         }*/
-        
-        if (Mathf.Abs(transform.position.x-Player1.transform.position.x)<10f)
+
+        if (Mathf.Abs(transform.position.x - Player1.transform.position.x) < 10f)
         {
-            transform.position=transform.position;
-            running=false;
+            transform.position = transform.position;
+            running = false;
             //ataque=true;
         }
-        else if (Mathf.Abs(transform.position.x-Player1.transform.position.x)<50f)
+        else if (Mathf.Abs(transform.position.x - Player1.transform.position.x) < 50f)
         {
-            transform.position=new Vector3(posX*distancia,transform.position.y,transform.position.z);
-            running=true;
-            ataque=false;
+            transform.position = new Vector3(posX * distancia, transform.position.y, transform.position.z);
+            running = true;
+            ataque = false;
         }
         else
         {
             //reanim=true;
-            transform.position=transform.position;            
-            running=false;
-            ataque=false;
+            transform.position = transform.position;
+            running = false;
+            ataque = false;
         }
-        if (transform.position.x-Player1.transform.position.x<0)
+        if (transform.position.x - Player1.transform.position.x < 0)
         {
-            transform.localScale= new Vector3(-1,1,1);
+            transform.localScale = new Vector3(-1, 1, 1);
         }
         else
         {
-            transform.localScale= new Vector3(1,1,1);
+            transform.localScale = new Vector3(1, 1, 1);
         }
-    }
+
+        //ataque V
+        if (tiempoEntreAtaque <= 0)   //CD para los ataques
+        {
+            Collider2D[] enemigosDañados = Physics2D.OverlapCircleAll(PosAtaque.position, Rango, que_es_objetivo);
+            for (int i = 0; i < enemigosDañados.Length; i++)
+            {
+                if (enemigosDañados.Length > 0)
+                {
+                    tiempoEntreAtaque = IniciartiempoEntreAtaque;
+                    enemigosDañados[i].GetComponent<player>().RecibeDaño(daño);
+                }
+            }
+        }
+        else
+        {
+            tiempoEntreAtaque -= Time.deltaTime;
+        }
+
+    }//este corchete cierra el void
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.CompareTag("AttackArea")||col.CompareTag("ground_ver"))
@@ -106,11 +138,23 @@ public class SpiderFollow : MonoBehaviour
             Debug.Log("ataque");
             spriteRenderer.material=MatBlanco;
             Invoke("resetMat",0.2f);
+            RecibeDaño(2);
         }
     }
     void resetMat()
     {
         spriteRenderer.material=MatDefault;
     }
-    
+    public void RecibeDaño(int d)         //cuando recibe daño
+    {
+        salud -= d;
+        Debug.Log("daño inflijido");
+        if (salud <= 0)                      //si pierde toda su vida
+        {
+            Destroy(this.gameObject);            //muere *agreguen animaciones porfa*
+        }
+        float proporcion = (float)salud / (float)salud_inicial;
+        vida.fillAmount = proporcion;
+    }
+
 }
