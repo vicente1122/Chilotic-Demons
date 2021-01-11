3c0c8289151fb104db0772f9fcc705f7260c038c
vicente1122 <1122viluoz@gmail.com>
2021-01-11 16:48:43 -0300

    ultimo

---
+++ Assets/cofre.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cofre : MonoBehaviour
+{
+    public GameObject mensaje;
+    public bool puedeabrir;
+    public KeyCode AbrirCofre;
+    public GameObject items;
+    public player p1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(AbrirCofre)&&puedeabrir)
+     {
+        items.gameObject.SetActive(true);
+        puedeabrir=false;
+        mensaje.gameObject.SetActive(false);
+     }   
+    }
+    void OnTriggerEnter2D()
+    {
+        mensaje.gameObject.SetActive(true);
+        puedeabrir=true;
+    }
+    void OnTriggerExit2D()
+    {
+        mensaje.gameObject.SetActive(false);
+        puedeabrir=false;
+    }
+}
